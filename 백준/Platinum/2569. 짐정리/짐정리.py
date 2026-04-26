432b1e0ae576cc39c5489fa25d9b8cb12726fe99
holelack <holelack.py@gmail.com>
2026-04-26 23:58:57 +0900

    [Platinum I] Title: 짐정리, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Platinum/2569. 짐정리/짐정리.py
@@ -0,0 +1,25 @@
+N = int(input())
+load = [int(input()) for _ in range(N)]
+_min = min(load)
+
+_sorted = sorted(load)
+idx = {i : idx for idx,i in enumerate(_sorted)}
+cmp = [idx[i] for i in load]
+
+visited = [0] * N
+ans = 0
+for i in range(N):
+    if visited[i]: continue
+    cur = i
+    cycle = [_sorted[cur]]
+    visited[cur] = 1
+    while cmp[cur] != i:
+        cur = cmp[cur]
+        visited[cur] = 1
+        cycle.append(_sorted[cur])
+
+    swap_min = 2*(_min + min(cycle)) + (len(cycle) - 1) * _min
+    not_swap_min = (len(cycle) - 1) * min(cycle)
+    ans += sum(cycle) - min(cycle) + min(swap_min, not_swap_min)
+
+print(ans)
